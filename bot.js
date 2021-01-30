18263c9d331e18ff3a17d691f7bba43739f3a1d9
toki0179 <47860095+olliwes@users.noreply.github.com>
2021-01-30 03:56:28 +0000

    Added Sharding

---
+++ bot.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { token } = require('./config.json');
+const db = require('quick.db');
+const { defaultCipherList } = require('constants');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	let prefix = db.get(`prefix_${message.guild.id}`)
+
+	const defaultprefix = '-'
+
+	if(prefix === null) prefix = defaultprefix
+
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).run(client, message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { token } = require('./config.json');
-const db = require('quick.db');
-const { defaultCipherList } = require('constants');
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: 'Nzk4NjEyMjA2NDQ2OTY4ODUy.X_3jkw.YtUGCW4zVkUKxt4AzfYkYDhIvMA' });
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('message', message => {
-	let prefix = db.get(`prefix_${message.guild.id}`)
-
-	const defaultprefix = '-'
-
-	if(prefix === null) prefix = defaultprefix
-
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).run(client, message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
-
-client.login(token);
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
