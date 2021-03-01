52ad3fb82624b799d7f99cfce69af9348dabea12
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-01 17:48:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//Do not edit any of this if you do not understand it
+
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config');
 
 const manager = new ShardingManager('./bot.js', {
     token: token,
     totalShards: "auto",
+    shardlist: "auto",
     respawn: false
 });
 
