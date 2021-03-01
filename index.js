be2e31687998f6fdc39558bdd0592461cd5e207f
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-01 17:52:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-//Do not edit any of this if you do not understand it
-
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config');
 
 const manager = new ShardingManager('./bot.js', {
     token: token,
-    totalShards: "auto",
-    shardList: "auto",
-    respawn: false
+    totalShards: "auto", //change this to the amount of shards
+    shardList: "auto", //change this to an array or keep at auto
+    respawn: false // change this to true if you would like shards automatically respawn upon exiting
 });
 
 manager.on("shardCreate", shard => console.log(`[ ShardManager ] Launching shard #${shard.id}`));
