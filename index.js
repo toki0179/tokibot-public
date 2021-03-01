55931e4e22f09d22d3e540b73ecb99547d25e588
toki0179 <47860095+olliwes@users.noreply.github.com>
2021-03-01 15:22:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'token-goes-here' });
+const { token } = require('./config');
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+const manager = new ShardingManager('./bot.js', {
+    token: token,
+    totalShards: "auto",
+    respawn: false
+});
+
+manager.on("shardCreate", shard => console.log(`[ ShardManager ] Launching shard #${shard.id}`));
 manager.spawn();
